cf9b2572d336fcd596cc5500c79310a59aaef68c
oss6 <ossedb@gmail.com>
2015-04-12 15:19:39 +0100

    Added docs

--- README.md
+++ README.md
@@ -6,6 +6,341 @@
 $ npm install --save fold
 ```
 
-## Examples
-
 ## Documentation
+- [foldL](#foldL)
+- [foldR](#foldR)
+
+* * *
+
+### foldL(arr, f, init)
+
+Higher-order function applied to arrays, based on the concept of accumulators
+(i.e. return a value from the accumulation). Left associative, tail recursive.
+
+**Parameters**
+
+**arr**: `Array`, Array to work on
+
+**f**: `function`, Function to apply ('a -> 'b -> 'a)
+
+**init**: `*`, Initial value
+
+**Returns**: `*`
+
+
+### foldR(arr, f, init)
+
+Higher-order function applied to arrays, based on the concept of accumulators
+(i.e. return a value from the accumulation). Right associative, not tail recursive.
+
+**Parameters**
+
+**arr**: `Array`, Array to work on
+
+**f**: `function`, Function to apply ('a -> 'b -> 'a)
+
+**init**: `*`, Initial value
+
+**Returns**: `*`, The accumulated value
+
+
+### and(arr)
+
+Applies the and operator to all the boolean expressions
+
+**Parameters**
+
+**arr**: `Array`, Array of boolean expressions
+
+**Returns**: `Boolean`
+
+
+### any(arr, f)
+
+Checks if a predicate is satisfied by any item
+
+**Parameters**
+
+**arr**: `Array`, Checks if a predicate is satisfied by any item
+
+**f**: `function`, The predicate
+
+**Returns**: `Boolean`
+
+
+### average(arr)
+
+Gets the average of a list of numbers
+
+**Parameters**
+
+**arr**: `Array`, Gets the average of a list of numbers
+
+**Returns**: `number`
+
+
+### composition(arr)
+
+Merge a list of functions into one function
+
+**Parameters**
+
+**arr**: `Array`, Array of functions
+
+**Returns**: `function`
+
+
+### count(arr, e)
+
+Counts the number of occurrences of an element
+
+**Parameters**
+
+**arr**: `Array`, Counts the number of occurrences of an element
+
+**e**: `*`, The element to check
+
+**Returns**: `Number`
+
+
+### drop(arr, n)
+
+Drops every 'n' times from 'arr'
+
+**Parameters**
+
+**arr**: `Array`, Drops every 'n' times from 'arr'
+
+**n**: `Number`, Drops every 'n' times from 'arr'
+
+**Returns**: `Array`, New array with the items deleted
+
+
+### filter(arr, f)
+
+Filter the array based on a given property
+
+**Parameters**
+
+**arr**: `Array`, Filter the array based on a given property
+
+**f**: `function`, The property/predicate to filter on
+
+**Returns**: `Array`, The filtered array
+
+
+### flatten(arr, deep)
+
+Flatten an array of arrays
+
+**Parameters**
+
+**arr**: `Array`, Array of arrays
+
+**deep**: `Boolean`, Whether to perform a 'deep flattening'
+
+**Returns**: `Array`, The flattened array
+
+
+### forEach(arr, f)
+
+Performs a for loop
+
+**Parameters**
+
+**arr**: `Array`, Performs a for loop
+
+**f**: `function`, The action to apply
+
+
+
+### insertionSort(arr)
+
+Performs an insertion sort
+
+**Parameters**
+
+**arr**: `Array`, The array to sort
+
+**Returns**: `Array`, The sorted array
+
+
+### join(arr, optional)
+
+Join the items of an array into a string
+
+**Parameters**
+
+**arr**: `Array`, Join the items of an array into a string
+
+**optional**: `String`, sep The separator. Default: ','
+
+**Returns**: `String`, The resulting string
+
+
+### map(arr, f)
+
+Creates a new array with the results of calling a provided function on every element
+
+**Parameters**
+
+**arr**: `Array`, Creates a new array with the results of calling a provided function on every element
+
+**f**: `function`, Creates a new array with the results of calling a provided function on every element
+
+**Returns**: `Array`, The new array
+
+
+### mapFusion(arr, fs)
+
+Performs multiple maps
+
+**Parameters**
+
+**arr**: `Array`, Performs multiple maps
+
+**fs**: `Array`, Array of functions to map
+
+**Returns**: `Array`, The new array
+
+
+### max(arr)
+
+Get the maximum item
+
+**Parameters**
+
+**arr**: `Array`, Get the maximum item
+
+**Returns**: `*`
+
+
+### member(arr, e)
+
+Checks whether 'e' is in 'arr'
+
+**Parameters**
+
+**arr**: `Array`, Checks whether 'e' is in 'arr'
+
+**e**: `*`, The element to test
+
+**Returns**: `Boolean`
+
+
+### min(arr)
+
+Get the minimum item
+
+**Parameters**
+
+**arr**: `Array`, Get the minimum item
+
+**Returns**: `*`
+
+
+### negateAll(arr)
+
+Negate all numbers in the array
+
+**Parameters**
+
+**arr**: `Array`, Negate all numbers in the array
+
+**Returns**: `Array`
+
+
+### or(arr)
+
+Applies the or operator to all the boolean expressions
+
+**Parameters**
+
+**arr**: `Array`, Array of boolean expressions
+
+**Returns**: `Boolean`
+
+
+### partition(arr, f)
+
+Partitions the array the array over a property
+
+**Parameters**
+
+**arr**: `Array`, Partitions the array the array over a property
+
+**f**: `function`, Partitions the array the array over a property
+
+**Returns**: `Object`, Object with key 'T' corresponding to all elements satisfying the property. 'F' otherwise.
+
+
+### removeConsecutive(arr)
+
+Remove consecutive items from an array
+
+**Parameters**
+
+**arr**: `Array`, Remove consecutive items from an array
+
+**Returns**: `Array`, The array without consecutive values
+
+
+### replicate(arr, n)
+
+Replicate the items 'n' times
+
+**Parameters**
+
+**arr**: `Array`, Replicate the items 'n' times
+
+**n**: `Number`, The replication amount
+
+**Returns**: `Array`
+
+
+### reverse(arr)
+
+Reverse an array
+
+**Parameters**
+
+**arr**: `Array`, Reverse an array
+
+**Returns**: `Array`, The reversed array
+
+
+### size(arr)
+
+Get the length of an array
+
+**Parameters**
+
+**arr**: `Array`, Get the length of an array
+
+**Returns**: `Number`, Length
+
+
+### sum(arr)
+
+Get the sum of an array of numbers
+
+**Parameters**
+
+**arr**: `Array`, Get the sum of an array of numbers
+
+**Returns**: `Number`
+
+
+### take(arr, n)
+
+Take just the elements at the nth position
+
+**Parameters**
+
+**arr**: `Array`, Take just the elements at the nth position
+
+**n**: `Number`, nth position
+
+**Returns**: `Array`
+
+* * *
--- fold.js
+++ fold.js
@@ -4,6 +4,10 @@ if(typeof exports === 'undefined'){
     var exports = window.fold = {};
 }
 
+// --- fold.js ---
+// A collection of useful functions using exclusively the 'fold'/'reduce' pattern
+// Ossama Edbali
+
 (function ($f) {
 
     var isEmpty = function (arr) {
@@ -75,34 +79,59 @@ if(typeof exports === 'undefined'){
         }
     };
 
-    // API
-
+    /**
+     * Higher-order function applied to arrays, based on the concept of accumulators
+     * (i.e. return a value from the accumulation). Left associative, tail recursive.
+     * @param {Array} arr - Array to work on
+     * @param {Function} f - Function to apply ('a -> 'b -> 'a)
+     * @param {*} init - Initial value
+     * @returns {*}
+     */
     $f.foldL = function _foldL (arr, f, init) {
         if (isEmpty(arr)) {
             return init;
         }
         else {
             var p = dctor(arr);
-            return f(_foldL(p.tl, f, init), p.hd);
+            return _foldL(p.tl, f, f(init, p.hd));
         }
     };
 
+    /**
+     * Higher-order function applied to arrays, based on the concept of accumulators
+     * (i.e. return a value from the accumulation). Right associative, not tail recursive.
+     * @param {Array} arr - Array to work on
+     * @param {Function} f - Function to apply ('a -> 'b -> 'a)
+     * @param {*} init - Initial value
+     * @returns {*} The accumulated value
+     */
     $f.foldR = function _foldR (arr, f, init) {
         if (isEmpty(arr)) {
             return init;
         }
         else {
             var p = dctor(arr);
-            return _foldR(p.tl, f, f(init, p.hd));
+            return f(_foldR(p.tl, f, init), p.hd);
         }
     };
 
+    /**
+     * Applies the and operator to all the boolean expressions
+     * @param {Array} arr Array of boolean expressions
+     * @returns {Boolean}
+     */
     $f.and = function (arr) {
         return $f.foldL(arr, function (a, x) {
             return a && x;
         }, true);
     };
 
+    /**
+     * Checks if a predicate is satisfied by any item
+     * @param {Array} arr
+     * @param {Function} f The predicate
+     * @returns {Boolean}
+     */
     $f.any = function (arr, f) {
         return $f.foldR(arr, function (a, x) {
             if (!a && f(x)) {
@@ -112,24 +141,46 @@ if(typeof exports === 'undefined'){
         }, false);
     };
 
+    /**
+     * Gets the average of a list of numbers
+     * @param {Array} arr
+     * @returns {number}
+     */
     $f.average = function (arr) {
         return arr.length > 0 ? $f.sum(arr) / arr.length : null;
     };
 
+    /**
+     * Merge a list of functions into one function
+     * @param {Array} arr Array of functions
+     * @returns {Function}
+     */
     $f.composition = function (arr) {
-        return $f.foldL(arr, compose, identity);
+        return $f.foldR(arr, compose, identity);
     };
 
+    /**
+     * Counts the number of occurrences of an element
+     * @param {Array} arr
+     * @param {*} e The element to check
+     * @returns {Number}
+     */
     $f.count = function (arr, e) {
         return $f.foldR(arr, function (a, x) {
             return x === e ? a + 1 : a;
         }, 0);
     };
 
+    /**
+     * Drops every 'n' times from 'arr'
+     * @param {Array} arr
+     * @param {Number} n
+     * @returns {Array} New array with the items deleted
+     */
     $f.drop = function (arr, n) {
         var count = 1;
 
-        return $f.foldR(arr, function (a, x) {
+        return $f.foldL(arr, function (a, x) {
             if (count % n !== 0) {
                 a.push(x);
             }
@@ -139,8 +190,14 @@ if(typeof exports === 'undefined'){
         }, []);
     };
 
+    /**
+     * Filter the array based on a given property
+     * @param {Array} arr
+     * @param {Function} f The property/predicate to filter on
+     * @returns {Array} The filtered array
+     */
     $f.filter = function (arr, f) {
-        return $f.foldR(arr, function (a, x) {
+        return $f.foldL(arr, function (a, x) {
             if (f(x)) {
                 a.push(x);
             }
@@ -149,86 +206,153 @@ if(typeof exports === 'undefined'){
         }, []);
     };
 
+    /**
+     * Flatten an array of arrays
+     * @param {Array} arr Array of arrays
+     * @param {Boolean} deep Whether to perform a 'deep flattening'
+     * @returns {Array} The flattened array
+     */
     $f.flatten = function _flatten (arr, deep) {
-        if (deep === undefined)
+        if (deep === undefined) {
             deep = false;
+        }
 
-        return $f.foldR(arr, function (a, x) {
+        return $f.foldL(arr, function (a, x) {
             a.pushArray(deep && isArray(x) ? _flatten(x) : x);
             return a;
         }, []);
     };
 
+    /**
+     * Performs a for loop
+     * @param {Array} arr
+     * @param {Function} f The action to apply
+     */
     $f.forEach = function (arr, f) {
-        return $f.foldR(arr, function (a, x) { f(x); }, undefined);
+        return $f.foldL(arr, function (a, x) { f(x); }, undefined);
     };
 
+    /**
+     * Performs an insertion sort
+     * @param {Array} arr The array to sort
+     * @returns {Array} The sorted array
+     */
     $f.insertionSort = function (arr) {
         return $f.foldL(arr, insert, []);
     };
 
+    /**
+     * Join the items of an array into a string
+     * @param {Array} arr
+     * @param {String} [optional] sep The separator. Default: ','
+     * @returns {String} The resulting string
+     */
     $f.join = function (arr, sep) {
         var count = 0;
         sep = sep || ',';
 
-        return $f.foldR(arr, function (a, x) {
+        return $f.foldL(arr, function (a, x) {
             return count++ > 0 ? a + sep + x : x;
         }, '');
     };
 
+    /**
+     * Creates a new array with the results of calling a provided function on every element
+     * @param {Array} arr
+     * @param {Function} f
+     * @returns {Array} The new array
+     */
     $f.map = function (arr, f) {
         f = f || identity;
 
-        return $f.foldR(arr, function (a, x) {
+        return $f.foldL(arr, function (a, x) {
             a.push(f(x));
             return a;
         }, []);
     };
 
+    /**
+     * Performs multiple maps
+     * @param {Array} arr
+     * @param {Array} fs Array of functions to map
+     * @returns {Array} The new array
+     */
     $f.mapFusion = function (arr, fs) {
         return $f.map(arr, $f.composition(fs));
     };
 
+    /**
+     * Get the maximum item
+     * @param {Array} arr
+     * @returns {*}
+     */
     $f.max = function (arr) {
         if (isEmpty(arr)) {
             return null;
         }
 
-        return $f.foldR(arr, function (a, x) {
+        return $f.foldL(arr, function (a, x) {
             return x > a ? x : a;
         }, dctor(arr).hd);
     };
 
+    /**
+     * Checks whether 'e' is in 'arr'
+     * @param {Array} arr
+     * @param {*} e The element to test
+     * @returns {Boolean}
+     */
     $f.member = function (arr, e) {
-        return $f.foldR(arr, function (a, x) {
+        return $f.foldL(arr, function (a, x) {
             return x === e ? true : a;
         }, false);
     };
 
+    /**
+     * Get the minimum item
+     * @param {Array} arr
+     * @returns {*}
+     */
     $f.min = function (arr) {
         if (isEmpty(arr)) {
             return null;
         }
 
-        return $f.foldR(arr, function (a, x) {
+        return $f.foldL(arr, function (a, x) {
             return x < a ? x : a;
         }, dctor(arr).hd);
     };
 
+    /**
+     * Negate all numbers in the array
+     * @param {Array} arr
+     * @returns {Array}
+     */
     $f.negateAll = function (arr) {
         return $f.map(arr, function (x) {
             return -x;
         });
     };
 
+    /**
+     * Applies the or operator to all the boolean expressions
+     * @param {Array} arr Array of boolean expressions
+     * @returns {Boolean}
+     */
     $f.or = function (arr) {
         return $f.foldL(arr, function (a, x) {
             return a || x;
         }, false);
     };
 
+    /**
+     * Partitions the array the array over a property
+     * @param {Array} arr
+     * @param {Function} f
+     * @returns {Object} Object with key 'T' corresponding to all elements satisfying the property. 'F' otherwise.
+     */
     $f.partition = function (arr, f) {
-        return $f.foldR(arr, function (a, x) {
+        return $f.foldL(arr, function (a, x) {
 
             if (f(x)) {
                 a.T.push(x);
@@ -242,6 +366,11 @@ if(typeof exports === 'undefined'){
         }, {'T': [], 'F': []});
     };
 
+    /**
+     * Remove consecutive items from an array
+     * @param {Array} arr
+     * @returns {Array} The array without consecutive values
+     */
     $f.removeConsecutive = function (arr) {
         if (isEmpty(arr)) {
             return arr;
@@ -250,7 +379,7 @@ if(typeof exports === 'undefined'){
         var cur = dctor(arr).hd,
             count = 0;
 
-        return $f.foldR(arr, function (a, x) {
+        return $f.foldL(arr, function (a, x) {
             if (count === 0) {
                 a.push(x);
             }
@@ -266,33 +395,60 @@ if(typeof exports === 'undefined'){
         }, []);
     };
 
+    /**
+     * Replicate the items 'n' times
+     * @param {Array} arr
+     * @param {Number} n The replication amount
+     * @returns {Array}
+     */
     $f.replicate = function (arr, n) {
-        return $f.foldR(arr, function (a, x) {
+        return $f.foldL(arr, function (a, x) {
             a.pushArray(fill(x, n));
 
             return a;
         }, []);
     };
 
+    /**
+     * Reverse an array
+     * @param {Array} arr
+     * @returns {Array} The reversed array
+     */
     $f.reverse = function (arr) {
-        return $f.foldL(arr, function (a, x) {
+        return $f.foldR(arr, function (a, x) {
             a.push(x);
             return a;
         }, []);
     };
 
+    /**
+     * Get the length of an array
+     * @param {Array} arr
+     * @returns {Number} Length
+     */
     $f.size = function (arr) {
         return $f.foldL(arr, function (a, x) { return a + 1; }, 0);
     };
 
+    /**
+     * Get the sum of an array of numbers
+     * @param {Array} arr
+     * @returns {Number}
+     */
     $f.sum = function (arr) {
         return $f.foldL(arr, add, 0);
     };
 
+    /**
+     * Take just the elements at the nth position
+     * @param {Array} arr
+     * @param {Number} n nth position
+     * @returns {Array}
+     */
     $f.take = function (arr, n) {
         var count = 1;
 
-        return $f.foldR(arr, function (a, x) {
+        return $f.foldL(arr, function (a, x) {
             if (count % n === 0) {
                 a.push(x);
             }
